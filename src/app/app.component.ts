17986d722733f798d60fc82a606ef9e48048c002
Rolando Escalona <rolando.escalona@gmail.com>
2017-10-31 10:08:30 -0600

    Book project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  dataset:any = [];
+
+  constructor(){};
+  
+  ngOnInit(){
+    console.log('on init');
+
+    d3.csv('./assets/books.csv', (data) => { 
+      this.dataset  = data;
+      console.log(data);
+    });
+
+  }// ngOnInit
 }
